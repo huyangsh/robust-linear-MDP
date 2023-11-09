Fix numerical issues.

agent/LinearPolicyGradient.py
@@ -5,7 +5,7 @@ from . import Agent
 
 
 class LinearPolicyGradientAgent(Agent):
-    def __init__(self, env, eps, eta, T_est, T_Q, thres):
+    def __init__(self, env, eta, T_est, T_Q, thres):
         # Environment information.
         self.env            = env
         
@@ -16,7 +16,6 @@ class LinearPolicyGradientAgent(Agent):
 
         self.reward         = env.reward
         self.gamma          = env.gamma
-        self.eps            = eps
 
         # Learning parameters.
         self.eta    = eta
@@ -36,14 +35,7 @@ class LinearPolicyGradientAgent(Agent):
         return self.pi
     
     def update(self):
-        Q_pi, phi_pi = self.env.robust_Q(pi=self.pi, T=self.T_Q, eps=self.eps)
-        
-        '''
-        Q_ref = self.env.DP_Q(pi=self.pi)
-        if np.linalg.norm((Q_pi-Q_ref).flatten()*self.pi.flatten()) > 1e-2:
-            print("\n", self.eps, self.pi, Q_pi, Q_ref)
-            input("WARNING > ")
-        '''
+        Q_pi, phi_pi = self.env.robust_Q(pi=self.pi, T=self.T_Q)
 
         # d_pi = self.env.visit_freq(self.pi, T=self.T_est)[:, np.newaxis]
         d_pi = self.env.robust_visit_freq(pi=self.pi, phi=phi_pi, T=self.T_est)[:, np.newaxis]
@@ -55,7 +47,7 @@ class LinearPolicyGradientAgent(Agent):
         for s in self.env.states:
             self.pi[s, :] = self._project(self.pi[s, :])
         
-        return self.pi, {"Q_pi": Q_pi}
+        return self.pi, {"Q_pi": Q_pi, "phi_pi": phi_pi}
 
     def select_action(self, state):
         return random.choices(self.actions, weights=self.pi[int(state),:])[0]
@@ -68,23 +60,41 @@ class LinearPolicyGradientAgent(Agent):
     
 
     # Utility: projection onto the probability simplex.
-    def _l2_project(self, r, a, b):
-        # Implements l2-projection onto the simplex:
-        #   min_y  ||r-x||^2
+    def _l2_project_bounded(self, v, a, b):
+        # Implements l2-projection onto the simplex with bounds:
+        #   min_x  ||v-x||^2
         #   s.t.   1^T x = 1
         #          a <= x <= b
         # REF: http://www.ryanhmckenna.com/2019/10/projecting-onto-probability-simplex.html.
         assert (a.sum()<=1) and (b.sum()>=1) and np.all(a<=b), "Error: projection infeasible."
-        lambdas = np.append(a-r, b-r)
+        lambdas = np.append(a-v, b-v)
         idx = np.argsort(lambdas)
         lambdas = lambdas[idx]
-        active = np.cumsum((idx < r.size)*2 - 1)[:-1]
+        active = np.cumsum((idx < v.size)*2 - 1)[:-1]
         diffs = np.diff(lambdas, n=1)
         totals = a.sum() + np.cumsum(active*diffs)
         i = np.searchsorted(totals, 1.0)
         lam = (1 - totals[i]) / active[i] + lambdas[i+1]
-        return np.clip(r + lam, a, b)
+        return np.clip(v + lam, a, b)
+    
+    def _l2_project(self, v):
+        # Implements l2-projection onto the simplex.
+        #   min_x  ||v-x||^2
+        #   s.t.   1^T x = 1
+        #          x >= 0
+        # REF: https://arxiv.org/abs/1309.1541 and https://stanford.edu/~jduchi/projects/DuchiShSiCh08.pdf.
+        # WARNING: numerical issues exist. Be sure to add numbers in similar magnitude first.
+        n = v.shape[0]
+        u = np.sort(v)[::-1]
+        u_sum = np.cumsum(u)
+        ind = np.arange(n) + 1
+        cond = (u - u_sum/ind + 1/ind) > 0
+        rho = ind[cond][-1]
+        lamda_1 = -u_sum[cond][-1] / float(rho)
+        lamda_2 = 1 / float(rho)
+        return np.maximum(v + lamda_1 + lamda_2, 0)
 
     def _project(self, x):
         assert x.shape == (self.env.num_actions,)
-        return self._l2_project(x, np.zeros_like(x), np.ones_like(x))
+        # return self.projection_simplex_sort(x,)
+        return self._l2_project(x)

env/LinearMDP.py
@@ -112,6 +112,11 @@ class LinearMDP(Env):
         
         return Q
     
+    def Q_to_V(self, Q, pi):
+        assert Q.shape == (self.num_states, self.num_actions)
+        assert pi.shape == (self.num_states, self.num_actions)
+        return np.sum(Q*pi, axis=1)
+    
     def Q_to_pi(self, Q):
         assert Q.shape == (self.num_states, self.num_actions)
         indices = np.argmax(Q, axis=1)[:,np.newaxis]
@@ -127,7 +132,7 @@ class LinearMDP(Env):
     
 
     # Utility: robust policy evaluation.
-    def robust_Q(self, pi, T, eps=0):
+    def robust_Q(self, pi, T):
         # Note that the following does not change across iterations.
         const_eta = np.sum((self.distr_init[:,np.newaxis] * pi)[:,:,np.newaxis] * self.phi, axis=(0,1))
         # The perturbation radius is still eps_phi.
@@ -136,7 +141,8 @@ class LinearMDP(Env):
         for t in range(T):
             # Note that the following does change across iterations.
             const_xi = self.theta + self.mu.T @ V_t
-            eps_xi = self.eps_theta + self.gamma* np.sum(abs(V_t)) * self.eps_mu
+            V_t_clipped = np.minimum(abs(V_t), np.ones(shape=(self.num_states,)) / self.gamma)
+            eps_xi = self.eps_theta + self.gamma* np.sum(V_t_clipped) * self.eps_mu
 
             # Optimization step: use the reduced form.
             def func(x):

env/utils.py
@@ -51,6 +51,26 @@ def get_linear_param(env_name):
         ])
         return distr_init, phi, theta, mu, mu_perturb
     elif env_name == "Mixture":
-        pass
+        distr_init = np.array([1/3,1/3,1/3], dtype=np.float32)
+        phi   = np.array([
+            [1,   0,   0],
+            [2/3, 1/3, 0],
+            [1/3, 2/3, 0],
+            [1/6, 1/2, 1/3],
+            [0,   1/2, 1/2],
+            [0,   0,   1]
+        ]).reshape((3,2,3))
+        theta = np.array([-1,1,0.2])
+        mu    = np.array([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ])
+        mu_perturb = np.array([
+            [1, 1/3, 0],
+            [0, 2/3, 0],
+            [0, 0, 1]
+        ])
+        return distr_init, phi, theta, mu, mu_perturb
     else:
         raise NotImplementedError

linear_PG_train.py
@@ -21,14 +21,21 @@ np.random.seed(seed)
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# Build environment
-gamma = 0.95
-eps = 0
+GAMMA     = 0.95
+EPS_PHI   = 0.01
+EPS_THETA = 0
+EPS_MU    = 0.05
 
+eta = 0.02
+T   = int(1e3)
+print(f"learning rate: {eta}.")
+
+
+# Build environment
 env_name = "Toy-4"
 distr_init, phi, theta, mu, mu_perturb = get_linear_param(env_name)
-env = LinearMDP(distr_init, phi, theta, mu, gamma, eps_phi=0.01, eps_theta=0.01, eps_mu=0.1, thres=1e-5)
-test_env = LinearMDP(distr_init, phi, theta, mu_perturb, gamma, thres=1e-5)
+env = LinearMDP(distr_init, phi, theta, mu, GAMMA, eps_phi=EPS_PHI, eps_theta=EPS_THETA, eps_mu=EPS_MU, thres=1e-5)
+test_env = LinearMDP(distr_init, phi, theta, mu_perturb, GAMMA, thres=1e-5)
 
 # Reference policies.
 pi_nominal = env.get_opt_pi()
@@ -36,11 +43,10 @@ pi_perturb = test_env.get_opt_pi()
 reward_nominal = test_env.distr_init @ test_env.DP_pi(pi=pi_nominal)
 reward_perturb = test_env.distr_init @ test_env.DP_pi(pi=pi_perturb)
 
-eta = 0.01
-T   = int(1e3)
-print(f"learning rate: {eta}.")
+tqdm.write(f"pi_nominal:\n{print_float_matrix(pi_nominal.T)}\nreward = {reward_nominal}.\n")
+tqdm.write(f"pi_perturb:\n{print_float_matrix(pi_perturb.T)}\nreward = {reward_perturb}.")
 
-agent = LinearPolicyGradientAgent(env, eps, eta, T_EST, T_Q, THRES)
+agent = LinearPolicyGradientAgent(env, eta, T_EST, T_Q, THRES)
 
 pi_init = np.ones(shape=(env.num_states, env.num_actions), dtype=np.float32) / env.num_actions
 agent.reset(pi_init)
@@ -49,7 +55,7 @@ eval_freq = 10
 try:
     reward_list, pi_list, t_list = [], [], []
     bar = tqdm(range(T))
-    bar.set_description_str(f"eta = {eta}, eps = {eps}")
+    bar.set_description_str(f"eta = {eta}, eps = ({EPS_PHI}, {EPS_THETA}, {EPS_MU}), target_reward = [{reward_nominal:.4g}, {reward_perturb:.4g}]")
     for t in bar:
         pi, info = agent.update()
         pi_list.append(pi)
@@ -58,6 +64,7 @@ try:
             tqdm.write(f"Evaluate at iteration #{t}:")
             tqdm.write(f"Q_pi:\n{print_float_matrix(info['Q_pi'].T)}")
             tqdm.write(f"pi:\n{print_float_matrix(pi.T)}")
+            tqdm.write(f"V_pi:\n{print_float_list(env.Q_to_V(info['Q_pi'],pi))}")
             t_list.append(t)
 
             test_reps = 10
@@ -74,22 +81,38 @@ try:
 except KeyboardInterrupt:
     pass
 
-fig = plt.figure(figsize=(20,5))
-t_horizon = list(range(len(pi_list)))
-for i in range(4):
-    ax = fig.add_subplot(1,4,i+1)
-    ax.plot(t_horizon, [pi[i,0] for pi in pi_list], label="left")
-    ax.plot(t_horizon, [pi[i,1] for pi in pi_list], label="stay")
-    ax.plot(t_horizon, [pi[i,2] for pi in pi_list], label="right")
-    ax.legend()
-    ax.set_xlabel(f"State {i}")
-    ax.set_ylabel(f"probability")
-fig.savefig(f"./log/linear_PG_{timestamp}_policy_{eps:.2f}_{eta:.2f}.png", dpi=300)
-
+# Plot rewards.
 fig = plt.figure(figsize=(5,5))
 ax = fig.add_subplot(1,1,1)
-ax.plot(t_list, reward_list, label=r"$\varepsilon=0.2$")
+ax.plot(t_list, reward_list, label=r"$\varepsilon=$" + f"{EPS_PHI, EPS_THETA, EPS_MU}")
 ax.axhline(y=reward_nominal, linestyle="--", color="r", label="nominal optimal")
 ax.axhline(y=reward_perturb, linestyle="--", color="g", label="perturbed optimal")
-fig.legend(loc="lower right")
-fig.savefig(f"./log/linear_PG_{timestamp}_reward_{eps:.2f}_{eta:.2f}.png", dpi=300)
+ax.legend(loc="lower right")
+fig.savefig(f"./log/linear_PG_{env_name}_{timestamp}_reward_{EPS_PHI:.3f}_{EPS_THETA:.3f}_{EPS_MU:.3f}_{GAMMA:.2f}_{eta:.2f}.png", dpi=300)
+
+# Plot policy changes over time.
+if env_name == "Toy-4":
+    fig = plt.figure(figsize=(20,5))
+    t_horizon = list(range(len(pi_list)))
+    for i in range(4):
+        ax = fig.add_subplot(1,4,i+1)
+        ax.plot(t_horizon, [pi[i,0] for pi in pi_list], label="left")
+        ax.plot(t_horizon, [pi[i,1] for pi in pi_list], label="stay")
+        ax.plot(t_horizon, [pi[i,2] for pi in pi_list], label="right")
+        ax.legend()
+        ax.set_xlabel(f"State {i}")
+        ax.set_ylabel(f"probability")
+    fig.savefig(f"./log/linear_PG_{env_name}_{timestamp}_policy_{EPS_PHI:.3f}_{EPS_THETA:.3f}_{EPS_MU:.3f}_{GAMMA:.2f}_{eta:.2f}.png", dpi=300)
+elif env_name == "Mixture":
+    fig = plt.figure(figsize=(20,5))
+    t_horizon = list(range(len(pi_list)))
+    for i in range(3):
+        ax = fig.add_subplot(1,3,i+1)
+        ax.plot(t_horizon, [pi[i,0] for pi in pi_list], label="high risk")
+        ax.plot(t_horizon, [pi[i,1] for pi in pi_list], label="low risk")
+        ax.legend()
+        ax.set_xlabel(f"State {i}")
+        ax.set_ylabel(f"probability")
+    fig.savefig(f"./log/linear_PG_{env_name}_{timestamp}_policy_{EPS_PHI:.3f}_{EPS_THETA:.3f}_{EPS_MU:.3f}_{GAMMA:.2f}_{eta:.2f}.png", dpi=300)
+else:
+    print("Policy plotting error: not implemented.")